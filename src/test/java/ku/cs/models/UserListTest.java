a342968a8d1bb84d176ff53385aad51c367f443b
Yuphadee <aomsinyuphadee@gmail.com>
2024-08-22 16:57:05 +0700

    Test findUser and changePassword

--- src/test/java/ku/cs/models/UserListTest.java
+++ src/test/java/ku/cs/models/UserListTest.java
@@ -11,21 +11,44 @@ class UserListTest {
     @DisplayName("User should be found in UserList")
     public void testUserListFindUser() {
         // TODO: add 3 users to UserList
+        User user1 = new User("Aomsin", "aomsin123");
+        User user2 = new User("Khaotung", "khaotung123");
+        User user3 = new User("First", "first123");
+
+        UserList userlist = new UserList();
+        userlist.addUser("Aomsin", "aomsin123");
+        userlist.addUser("Khaotung", "khaotung123");
+        userlist.addUser("First", "first123");
 
         // TODO: find one of them
+        User findUser = userlist.findUserByUsername("Aomsin");
+
 
         // TODO: assert that UserList found User
         // String expected = "<one of username>";
         // String actual = user.getUsername();
         // assertEquals(expected, actual);
+        String expected = "Aomsin";
+        String actual = findUser.getUsername();
+        assertEquals(expected, actual);
+
     }
 
     @Test
     @DisplayName("User can change password")
     public void testUserCanChangePassword() {
         // TODO: add 3 users to UserList
+        User user1 = new User("Aomsin", "aomsin123");
+        User user2 = new User("Khaotung", "khaotung123");
+        User user3 = new User("First", "first123");
+
+        UserList userlist = new UserList();
+        userlist.addUser("Aomsin", "aomsin123");
+        userlist.addUser("Khaotung", "khaotung123");
+        userlist.addUser("First", "first123");
 
         // TODO: change password of one user
+        userlist.changePassword("Aomsin", "aomsin123", "luvkhaofirst");
 
         // TODO: assert that user can change password
         // assertTrue(actual);
